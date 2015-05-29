b693e3edf29f928c88ac9aa81bc03fb328e3daab
Jurkan <->
2015-05-29 20:04:59 +0200

    Travis configuration changed

---
+++ .travis/upload.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+HOST='aero-club.eu'
+USER=$1
+PASS=$2
+TARGETFOLDER='public'
+SOURCEFOLDER='public'
+
+lftp -f "
+open $HOST
+user $USER $PASS
+lcd $SOURCEFOLDER
+mirror --reverse --delete --verbose $SOURCEFOLDER $TARGETFOLDER
+bye
+"
