1eca3c4608c74239da3e7955de1ca9a644b939f6
Jurkan <->
2015-05-29 21:20:20 +0200

    Travis Upload Skript nochmal angepasst

--- .travis/upload.sh
+++ .travis/upload.sh
@@ -2,13 +2,13 @@
 HOST='aero-club.eu'
 USER=$1
 PASS=$2
-TARGETFOLDER='public'
-SOURCEFOLDER='public'
+#TARGETFOLDER='public'
+SOURCEFOLDER='./public'
 
 lftp -f "
 open $HOST
 user $USER $PASS
 lcd $SOURCEFOLDER
-mirror --reverse --delete --verbose $SOURCEFOLDER $TARGETFOLDER
+mirror --reverse --delete --verbose $SOURCEFOLDER
 bye
 "
